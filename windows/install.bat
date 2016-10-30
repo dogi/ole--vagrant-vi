a9ccbdf2d68dd76672f66e7b6a37fa2cfa823bfc
dogi <stefan@unterhauser.name>
2016-10-29 22:14:29 -0400

    remove https firewall rule in install.bat

--- windows/install.bat
+++ windows/install.bat
@@ -62,14 +62,6 @@ echo Creating firewall rule CouchDB/HTTP(BeLL)+1
 netsh advfirewall firewall add rule name="CouchDB/HTTP(BeLL)+1" dir=out action=allow protocol=TCP localport=5985
 netsh advfirewall firewall add rule name="CouchDB/HTTP(BeLL)+1" dir=in action=allow protocol=TCP localport=5985
 
-netsh advfirewall firewall show rule name="CouchDB/HTTPS(BeLL)+1" >nul
-if not ERRORLEVEL 1 (
-	echo Ports are already open.
-	netsh advfirewall firewall delete rule name="CouchDB/HTTPS(BeLL)+1"
-) 
-echo Creating firewall rule CouchDB/HTTPS(BeLL)+1
-netsh advfirewall firewall add rule name="CouchDB/HTTPS(BeLL)+1" dir=out action=allow protocol=TCP localport=6985
-netsh advfirewall firewall add rule name="CouchDB/HTTPS(BeLL)+1" dir=in action=allow protocol=TCP localport=6985
 echo Installation completed. Vagrant is starting...
 pause
 exit
