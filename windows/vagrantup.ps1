661149936549111152418f92af1045deedcd234a
Antonella Bernobich Dean <aberdean@users.noreply.github.com>
2016-10-31 11:56:36 +0100

    fix bug in vagrantup.ps1 if running multiple machines

--- windows/vagrantup.ps1
+++ windows/vagrantup.ps1
@@ -3,7 +3,7 @@ Write-Host Starting BeLL App... -ForegroundColor Magenta
 C:\HashiCorp\Vagrant\bin\vagrant.exe up
 $str = C:\HashiCorp\Vagrant\bin\vagrant.exe global-status
 $strArr = @($str.Split(" "))
-if ($strArr.Item(($strArr.IndexOf("virtualbox")+1)) -eq "running") {
+if ($strArr.Item(($strArr.IndexOf("vi")+6)) -eq "running") {
     Write-Host You can now click on the MyBeLL icon on your desktop to launch your BeLL App. -ForegroundColor Magenta
 } else {
     Write-Host Your BeLL App virtual machine doesn`'t seem to be running properly. -ForegroundColor Magenta
